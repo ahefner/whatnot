271cf23bc5c7ee17957f8968f6cc6dd116ddcec6
Andy Hefner <ahefner@gmail.com>
2015-08-10 00:37:19 -0400

    Use option type to represent empty tree

--- ocaml/99/25.ml
+++ ocaml/99/25.ml
@@ -10,24 +10,20 @@ type 'a ctree =
   | Seg of int * 'a list
   | Subtree of int * 'a ctree * 'a ctree ;;
 
-type 'a tree =
-  | Tree of 'a ctree
-  | EmptyTree ;;
-
 let rec nthtail n = function
   | [] -> raise (InvalidArg "List can't be empty!")
   | x :: t when (n<=0) -> x,t
   | _ :: t -> nthtail (n-1) t ;;
 
-let ctreeLen = function
+let ctreeSize = function
   | Subtree (len,_,_) -> len
   | Seg (len,_) -> len ;;
 
-let makeSeg n t = if (n<=0) then EmptyTree else Tree (Seg (n,t)) ;;
+let makeSeg n t = if (n<=0) then None else Some (Seg (n,t)) ;;
 let makeSubtree = function
-  | (EmptyTree,EmptyTree) -> EmptyTree
-  | (EmptyTree,x) | (x,EmptyTree) -> x
-  | (Tree x, Tree y) -> Tree (Subtree ((ctreeLen x) + (ctreeLen y), x, y)) ;;
+  | (None,None) -> None
+  | (None,x) | (x,None) -> x
+  | (Some x, Some y) -> Some (Subtree ((ctreeSize x) + (ctreeSize y), x, y)) ;;
 
 let selSeg index = function
   | (n,x::t) when (index==0) -> x, makeSeg (n-1) t
@@ -36,19 +32,18 @@ let selSeg index = function
 let rec select index = function
   | Subtree (n,l,r) ->
      assert (index < n) ;
-     let lenl = ctreeLen l in
+     let lenl = ctreeSize l in
      if (index < lenl)
-     then let (x,nl) = (select index l) in (x, makeSubtree (nl, Tree r))
-     else let (x,nr) = (select (index-lenl) r) in (x, makeSubtree (Tree l, nr))
-  | Seg (n,t) -> selSeg index (n,t)
-;;
+     then let (x,nl) = (select index l) in (x, makeSubtree (nl, Some r))
+     else let (x,nr) = (select (index-lenl) r) in (x, makeSubtree (Some l, nr))
+  | Seg (n,t) -> selSeg index (n,t) ;;
 
 let makeTree (type a) = function
-  | [] -> EmptyTree
-  | x -> Tree (Seg ((List.length x), x)) ;;
+  | [] -> None
+  | x -> Some (Seg ((List.length x), x)) ;;
 
 let permutation list =
   let rec aux = function
-    | EmptyTree -> []
-    | Tree s -> let (x,t) = select (Random.int (ctreeLen s)) s in x :: aux t
+    | None -> []
+    | Some s -> let (x,t) = select (Random.int (ctreeSize s)) s in x :: aux t
   in aux (makeTree list) ;;
