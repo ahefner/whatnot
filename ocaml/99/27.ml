4b4a5b5bc8322c2f3471a4812465f6cf8b9f4131
Andy Hefner <ahefner@gmail.com>
2015-08-20 07:25:13 -0400

    Fix problem 27

--- ocaml/99/27.ml
+++ ocaml/99/27.ml
@@ -10,38 +10,25 @@ Generalize the above function in a way that we can specify a list of group sizes
  [["a"; "d"]; ["b"]]; [["b"; "d"]; ["a"]]; [["a"; "d"]; ["c"]];
  [["b"; "d"]; ["c"]]; [["c"; "d"]; ["a"]]; [["c"; "d"]; ["b"]]]
 
- [ What?? Example output looks wrong. None of the above are exhaustive
-   partitions of the four elements, there's always one missing.
-   Maybed I've solved an entirely different problem. No matter, I like
-   my problem better. ]
+(I don't think this problem description is clear, but refering to the
+original 99 Prolog problems clarified it. In my opinion this problem
+is mangled versus the original P-99 set, and the above example for
+'group' is particulrly bad.)
+
  *)
 
-let group list groups =
-  let rec aux accum = function
-    (* Successful partitioning of the list. Return one result. *)
-    | (m, _, 0, [], []) -> [accum::m]
-    (* Picked one subgroup. Elements remaining, so recurse on this and
-      next group spec. To avoid duplicate groups, recursing to build
-      another instance of the current group size chooses only from the
-      tail and not the already-skipped elements. The next group spec
-      is allowed to choose from both the tail and the skipped
-      elements. *)
-    | (m, g::h::ght, n, t, skipped) when (n <= 0) ->
-       aux [] (accum::m, g::h::ght, g, t, skipped) (* Same group *)
-       @ aux [] (accum::m, h::ght, h, skipped @ t, []) (* Next group type *)
-    (* Picked one subgroup, with elements remaining. Pick
-      another. Same as above, but no additional group sizes, so no
-      double recursion. *)
-    | (m, [g], n, t, skipped) when (n <= 0) ->
-       aux [] (accum::m, [g], g, t, skipped)
-    (* Insufficient elements to choose this group *)
-    | (_, _, n, [], _) (* when (n > 0) *) -> []
-    (* Recurse, continuing to accumulate this group *)
-    | (m, groups, n, x::t, skipped) ->
-       aux (x::accum) (m, groups, n-1, t, skipped)
-       @ aux accum (m, groups, n, t, x::skipped)
-  in match groups with
-     | [] -> []
-     | g::t -> aux [] ([], g::t, g, list, [])
-  ;;
+let extend x = function
+  | [] -> [[x]]
+  | a::s -> (x::a)::s ;;
 
+let rec aux = function
+  | ([], _, _, _) -> []
+  | ([0], acc, [], []) -> [acc]
+  | (0::g, acc, t, rem) -> aux (g, []::acc, rem @ t, [])
+  | (_,_,[],_) -> []
+  | (n::g as ng, acc, x::t, rem) ->
+     aux ((n-1)::g, (extend x acc), t, rem)
+     @ aux (ng, acc, t, x::rem) ;;
+
+let group items groups =
+  aux (groups, [], items, []) ;;
