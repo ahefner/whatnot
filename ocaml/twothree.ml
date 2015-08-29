6bfe84166fad0946f83c09b4a55632971f46029d
Andy Hefner <ahefner@gmail.com>
2015-08-29 05:59:11 -0400

    Implement insertion for 2-3 (playing with OCaml)
    That wasn't too bad. Strengthened representation to eliminate
    'nullability' of nodes which could violate invariants of the tree
    structure - distinguish empty trees disjointly from interior tree nodes,
    and distinguish 1 and 2 leaves from 2 and 3 interior nodes.
    
    Seems like the hairiness of special cases in order to implement 2-3
    trees is overstated, but I'll try a red-black tree to do a side-by-side
    comparison. Not sure it makes a difference that I'm doing it
    persistently, versus implementing rotations by mutating the tree (yuck).

--- ocaml/twothree.ml
+++ ocaml/twothree.ml
@@ -1,46 +1,86 @@
 (* Crude persistent 2-3 Tree *)
 
-type 'a ttt =
-  | Two of 'a ttt * 'a * 'a ttt
-  | Three of 'a ttt * 'a * 'a ttt * 'a * 'a ttt
-  | Empty ;;
+(* Interior node or leaf of non-empty 2-3 tree *)
+type 'a ttnode =
+  | Two of 'a ttnode * 'a * 'a ttnode
+  | Three of 'a ttnode * 'a * 'a ttnode * 'a * 'a ttnode
+  | Leaf of 'a
+  | TwoLeaf of 'a * 'a  (* Not ordered *)
+;;
+
+(* 2-3 Tree *)
+type 'a ttt = Empty | Root of 'a ttnode ;;
 
 let rec find key = function
   | Empty -> false
-  | Two (_,x,_) when x = key -> true
-  | Two (l,x,r) -> if (key<x) then find key l else find key r
-  | Three (_,x,_,_,_) when x = key -> true
-  | Three (_,_,_,y,_) when y = key -> true
-  | Three (_,x,m,y,_) when (x < key && key < y) -> find key m
-  | Three (l,x,_,y,r) -> if (key < x) then find key l else find key r ;;
+  | Root n ->
+     let rec findin = function
+       | Two (_,x,_) when x = key -> true
+       | Two (l,x,r) -> if (key < x) then findin l else findin r
+       | Three (_,x,_,_,_) when x = key -> true
+       | Three (_,_,_,y,_) when y = key -> true
+       | Three (_,x,m,y,_) when (x < key && key < y) -> findin m
+       | Three (l,x,_,y,r) -> if (key < x) then findin l else findin r
+       | Leaf (x) -> key = x
+       | TwoLeaf (x,y) -> (key = x || key = y)
+     in findin n
+;;
 
-(* TODO: Implement insert/delete operations*)
+(* Recursive three element bubble sort? :) *)
+let rec sortedtriple x y z =
+  if x > y then sortedtriple y x z
+  else if y > z then sortedtriple x z y
+  else (x,y,z) ;;
 
-(* Tests  *)
+type 'a insertop = ReplaceWith of 'a ttnode | MergeUp of 'a ttnode * 'a * 'a ttnode ;;
 
-assert (true == find 1 (Two (Empty,1,Empty))) ;;
-assert (false == find 7 (Two (Empty,0,Empty))) ;;
+let joinleft = function
+  | (ReplaceWith l, x, r) -> ReplaceWith (Two (l,x,r))
+  | (MergeUp (l,x,m), y, r) -> ReplaceWith (Three (l,x,m,y,r)) ;;
 
-let tr1 = (Two
-             ((Three (Two (Empty,1,Empty),
-                     3,
-                     Two (Empty,5,Empty),
-                     7,
-                     Two (Empty,9,Empty))),
-              11,
-              (Two (Empty,15,Empty)))) ;;
+let joinright = function
+  | (l, x, ReplaceWith r) -> ReplaceWith (Two (l,x,r))
+  | (l, x, MergeUp (m,y,r)) -> ReplaceWith (Three (l,x,m,y,r)) ;;
 
-assert (not (find 0 tr1));;
-assert ((find 1 tr1));;
-assert (not (find 2 tr1));;
-assert ((find 3 tr1));;
-assert (not (find 4 tr1));;
-assert ((find 5 tr1));;
-assert (not (find 6 tr1));;
-assert ((find 7 tr1));;
-assert (not (find 8 tr1));;
-assert ((find 9 tr1));;
-assert (not (find 10 tr1));;
-assert ((find 11 tr1));;
-assert (not (find 12 tr1));;
-assert ((find 15 tr1));;
+let join2left = function
+  | (ReplaceWith l, x, m, y, r) -> ReplaceWith (Three (l,x,m,y,r))
+  | (MergeUp (l,x1,m1), x2, m2, y, r) -> MergeUp (Two (l,x1,m1), x2, Two (m2,y,r)) ;;
+
+let joinmiddle = function
+  | (l, x, ReplaceWith m, y, r) -> ReplaceWith (Three (l, x, m, y, r))
+  | (l, x1, MergeUp (m1,y,m2), z, r) -> MergeUp (Two (l,x1,m1), y, Two(m2,z,r)) ;;
+
+let join2right = function
+  | (l, x, m, y, ReplaceWith r) -> ReplaceWith (Three (l, x, m, y, r))
+  | (l, x, m1, y, MergeUp (m2,z,r)) -> MergeUp (Two (l,x,m1), y, Two (m2,z,r)) ;;
+
+let rec inserting key = function
+  | Leaf x as itself when x=key -> ReplaceWith itself
+  | TwoLeaf (x,y) as itself when x=key || y=key -> ReplaceWith itself
+  | Two (_,x,_) as itself when (key = x) -> ReplaceWith itself
+  | Three (_,x,_,y,_) as itself when x=key || y=key -> ReplaceWith itself
+  | Leaf x -> ReplaceWith (TwoLeaf (x,key))
+  | TwoLeaf (x,y) ->
+     let (x,y,z) = sortedtriple x y key
+     in MergeUp (Leaf x, y, Leaf z)
+  | Two (l,x,r) ->
+     if key < x then joinleft ((inserting key l), x, r)
+     else joinright (l, x, (inserting key r))
+  | Three (l,x,m,y,r) ->
+     if (key < x) then join2left ((inserting key l),x,m,y,r)
+     else if (key < y) then joinmiddle (l,x,(inserting key m),y,r)
+     else join2right (l,x,m,y,(inserting key r)) ;;
+
+let insert key = function
+  | Empty -> Root (Leaf key)
+  | Root root -> match inserting key root with
+                 | ReplaceWith newroot -> Root newroot
+                 | MergeUp (l,x,r) -> Root (Two (l,x,r)) ;;
+
+(* TODO: Implement delete operation*)
+
+(* TODO: Write tests *)
+
+
+
+
